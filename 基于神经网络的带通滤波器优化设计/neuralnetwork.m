9d2ca9f1509a601616d6f823516a892044d0f0f7
liujiangfan <951416168@qq.com>
2017-12-19 17:47:37 +0800

    Version 1.1

--- 基于神经网络的带通滤波器优化设计/neuralnetwork.m
+++ 基于神经网络的带通滤波器优化设计/neuralnetwork.m
@@ -16,7 +16,7 @@ net=newff(inputn,outputn,[5,10]);
 %设置隐藏层层数和每层神经元数目，输入输出层由数据确定。
 net.trainParam.epochs=1000;%最大迭代次数1000
 net.trainParam.lr=0.1;%学习率0.1
-net.trainParam.goal=0.001;%目标误差率
+net.trainParam.goal=0.0035;%目标误差率
 
 net=train(net,inputn,outputn);%训练设置好的神经网络模型
 
@@ -30,6 +30,7 @@ figure(1);
 plot(testinput(1,:),BPoutput(1:2,:),'.')%画出网络预测的S11,S12幅值的图像
 hold on;
 plot(testinput(1,:),output_test(1:2,:),'-')%画图仿真的S11，S12幅值的图像
+xlabel('frequency/GHz');ylabel('value/dB')
 legend('预测magS11','预测magS12','仿真magS11','仿真magS12')
 title('Simulated S by HFSS and predicted S by neural network')
 
@@ -37,6 +38,7 @@ figure(2);
 plot(testinput(1,:),BPoutput(3:4,:),'.')%画出网络预测的S11,S12相位的图像
 hold on;
 plot(testinput(1,:),output_test(3:4,:),'-')%画图仿真的S11，S12相位的图像
+xlabel('frequency/GHz');ylabel('value/deg')
 legend('预测degS11','预测degS12','仿真degS11','仿真degS12')
 title('Simulated S by HFSS and predicted S by neural network')
 
